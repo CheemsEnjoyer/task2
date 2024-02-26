0378c8b18181d741afbbf3aa53d2167af688f866
CheemsEnjoyer <95558369+CheemsEnjoyer@users.noreply.github.com>
2024-02-26 14:18:17 +0800

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxListOfNumbers = new System.Windows.Forms.TextBox();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxListOfNumbers
+            // 
+            this.textBoxListOfNumbers.Location = new System.Drawing.Point(195, 40);
+            this.textBoxListOfNumbers.Name = "textBoxListOfNumbers";
+            this.textBoxListOfNumbers.Size = new System.Drawing.Size(100, 22);
+            this.textBoxListOfNumbers.TabIndex = 0;
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(35, 93);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(100, 22);
+            this.textBoxN.TabIndex = 1;
+            this.textBoxN.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Последовательность чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "N";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 193);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.textBoxListOfNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxListOfNumbers;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTextBoxValues();
+        }
+
+        private void LoadTextBoxValues()
+        {
+            textBoxN.Text = Properties.Settings.Default.textBoxN;
+            textBoxListOfNumbers.Text = Properties.Settings.Default.textBoxListOfNumbers;
+        }
+
+        private void SaveTextBoxValues()
+        {
+            Properties.Settings.Default.textBoxN = textBoxN.Text;
+            Properties.Settings.Default.textBoxListOfNumbers = textBoxListOfNumbers.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxN.Text, out int n))
+            {
+                string[] numbers = textBoxListOfNumbers.Text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int[] sequence = new int[numbers.Length];
+                bool validInput = true;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (!int.TryParse(numbers[i], out sequence[i]))
+                    {
+                        validInput = false;
+                        break;
+                    }
+                }
+
+                if (validInput)
+                {
+                    string result = Logic.Check(sequence, n);
+                    MessageBox.Show(result);
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, введите только числа в поле 'Последовательность чисел'.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректное значение для n.");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTextBoxValues();
+        }
+    }
+    }
+
+public class Logic
+{
+    public static string Check(int[] sequence, int n)
+    {
+        string msg = "";
+        bool found = false;
+        int position = -1;
+
+        for (int i = 0; i < sequence.Length - n + 1; i++)
+        {
+            bool match = true;
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (sequence[i + j] != sequence[i + j + 1])
+                {
+                    match = false;
+                    break;
+                }
+            }
+            if (match)
+            {
+                found = true;
+                position = i + 1;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            if (n == 0)
+            {
+                msg = "Не может быть последовательности из 0 чисел.";
+            }
+            else
+            {
+                msg = $"В последовательности найдена {n} одинаковых соседних чисел, начиная с позиции {position}.";
+            }
+        }
+        else
+        {
+            msg = $"В последовательности нет {n} одинаковых соседних чисел.";
+        }
+        return msg;
+    }
+}
+
